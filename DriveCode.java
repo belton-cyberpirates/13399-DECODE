8abf6d7d2129aca6783f2482a9bca8a318c17131
owen <123703288+almodn@users.noreply.github.com>
2025-11-06 18:00:25 -0600

    upload drive code

---
+++ DriveCode.java
@@ -0,0 +1,123 @@
+//f-f-f-f-f-f-f-f-five nights at bear5s
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠤⠤⠀⢀⣤⣤⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⢠⠤⠟⢂⢘⡿⣿⣧⣄⡀⠀⠀⢀⠄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠨⠠⠌⠐⠀⠈⠀⢀⣒⠲⣿⡿⢂⣂⠠⣾⠶⠈⠀⠤⣀⠔⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣔⠦⠘⠛⠃⠈⣁⠘⠛⠁⢿⢿⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠎⣾⣷⣿⣿⣿⣶⣶⣿⣿⣿⣿⣮⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⡀⠀⠉⠉⠉⠙⠛⠙⠛⣛⢉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⣀⣀⣠⣴⣶⣾⣿⣿⣇⠀⠑⠂⠀⠀⠀⠀⠀⠀⠐⣀⠀⠘⠉⠁⢠⣤⣤⣀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⢃⣿⣽⣿⠁⣠⣴⣾⣿⣾⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠈⠽⣿⣿⣷⣆⠀⠀⠀⠀
+//⠀⠀⠀⠄⠈⠁⠀⠀⠀⢠⣉⠉⠩⠭⠐⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠉⠁⣀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠒⠚⠿⠭⢭⡅⠀⢀⣨⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⢲⣟⣿⣿⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠛⠛⠛⠛⠛⠛⠛⡛⠛⠋⠀⠀⠀⠀⠀⠀⠀⠠⡶⠄⢀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠀⠀⠈⠺⠍⠐⠐⡒⠉⠀⠀⠀⠔⠂⢙⣶⣿⣽⣿⡆⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡐⣶⣶⣄⠀⠀⠀⠀⠀⠀⣠⣴⣶⣶⠀⠀⠀⠀⠀⠈⠁⠈⠉⠛⠁⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢤⣝⣿⣿⣿⠟⠀⠀⠀⠀⢻⢿⢻⣿⣿⠾⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠀⠈⢛⣻⣿⠁⠀⠀⠀⠀⠀⠀⠈⣚⣿⣯⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣨⣵⣿⣿⠀⠀⠀⠀⠀⠀⠠⢭⣤⣭⡁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢬⣹⢿⣿⡏⠀⠀⠀⠀⠀⠀⠐⡿⣿⣿⢿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠨⠍⠉⠀⠀⠀⠀⠀⠀⠀⠡⠔⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⢠⡤⠀⢀⠀⣤⠄⢀⣠⣤⣄⠀⠀⠀⠀⢀⣤⣤⡀⠀⢀⣀⡄⠀⠤⣤⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠈⠉⠀⠀⠉⠉⠉⠁⠈⠉⠉⠉⠁⠀⠀⠀⠉⠉⠉⠁⠀⠉⠉⠉⠀⠀⠉⠉⠀⠀⠀⠀⠀
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp
+
+public class owentestcode extends LinearOpMode {
+
+    //static final int turnSpeed = 2000;
+    int driveSpeed = 800;
+    int boost = 0;
+
+    static final double flyWheelSpeed = .76;
+
+    private DcMotorEx turret;
+    private DcMotorEx flyWheelRight;
+    private DcMotorEx flyWheelLeft;
+    
+    private DcMotorEx intake;
+    
+    private CRServo outZero;
+    private CRServo outOne;
+
+    private DcMotorEx driveMotorBL;
+    private DcMotorEx driveMotorFL;
+    private DcMotorEx driveMotorBR;
+    private DcMotorEx driveMotorFR;
+
+    @Override
+    public void runOpMode() {
+        
+        intake = hardwareMap.get(DcMotorEx.class, "intake");
+        
+        outZero = hardwareMap.get(CRServo.class, "out0");
+        outOne = hardwareMap.get(CRServo.class, "out1");
+        
+        flyWheelRight = hardwareMap.get(DcMotorEx.class, "fly1");
+        flyWheelLeft = hardwareMap.get(DcMotorEx.class, "fly2");
+        
+        //turret = hardwareMap.get(DcMotorEx.class, "turret");
+        
+        driveMotorBL = hardwareMap.get(DcMotorEx.class, "driveBL");
+        driveMotorFL = hardwareMap.get(DcMotorEx.class, "driveFL");
+        driveMotorBR = hardwareMap.get(DcMotorEx.class, "driveBR");
+        driveMotorFR = hardwareMap.get(DcMotorEx.class, "driveFR");
+        
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        
+        driveMotorBL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        driveMotorBR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        driveMotorFL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        driveMotorFR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        
+        waitForStart();
+        
+        while (opModeIsActive()) {
+            
+            double oneRightStickX = - gamepad1.right_stick_x;
+            double oneRightStickY = gamepad1.right_stick_y;
+            double oneLeftStickX = gamepad1.left_stick_x;
+            
+            double twoLeftStickY = gamepad2.left_stick_y;
+            
+            if (gamepad1.right_bumper) {
+                boost = 1000;    
+            } else {
+                boost = 0;
+            }
+            
+            driveMotorBL.setVelocity((driveSpeed + boost) * -(oneRightStickX - oneRightStickY + oneLeftStickX));
+            driveMotorFL.setVelocity((driveSpeed + boost) * (oneRightStickX + oneRightStickY - oneLeftStickX));
+            driveMotorBR.setVelocity((driveSpeed + boost) * -(oneRightStickX + oneRightStickY + oneLeftStickX));
+            driveMotorFR.setVelocity((driveSpeed + boost) * (oneRightStickX - oneRightStickY - oneLeftStickX));
+            
+            flyWheelLeft.setPower(-twoLeftStickY * flyWheelSpeed);
+            flyWheelRight.setPower(twoLeftStickY * flyWheelSpeed);
+            
+            if (gamepad2.left_bumper) {
+                intake.setPower(-1);
+            } else {
+                intake.setPower(0);
+            }
+            
+             if (gamepad2.right_bumper) {
+                outZero.setPower(-1);
+                outOne.setPower(1);
+            } else {
+                outZero.setPower(0);
+                outOne.setPower(0);
+            }
+        }
+    }
+}

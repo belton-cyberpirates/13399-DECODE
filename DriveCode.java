c32bfe002a57d8a7cf0bb13d771d288878eb678a
owen <123703288+almodn@users.noreply.github.com>
2025-11-07 08:05:45 -0600

    add comments

--- DriveCode.java
+++ DriveCode.java
@@ -34,22 +34,21 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 @TeleOp
 
 public class owentestcode extends LinearOpMode {
-
-    //static final int turnSpeed = 2000;
+    //speeds
+    //static final int turretSpeed = 2000;
     int driveSpeed = 800;
     int boost = 0;
-
     static final double flyWheelSpeed = .76;
-
+    //outtake motors
     private DcMotorEx turret;
     private DcMotorEx flyWheelRight;
     private DcMotorEx flyWheelLeft;
-    
+    //intake
     private DcMotorEx intake;
-    
+    //servos
     private CRServo outZero;
     private CRServo outOne;
-
+    //drivemotors
     private DcMotorEx driveMotorBL;
     private DcMotorEx driveMotorFL;
     private DcMotorEx driveMotorBR;
@@ -59,15 +58,11 @@ public class owentestcode extends LinearOpMode {
     public void runOpMode() {
         
         intake = hardwareMap.get(DcMotorEx.class, "intake");
-        
         outZero = hardwareMap.get(CRServo.class, "out0");
         outOne = hardwareMap.get(CRServo.class, "out1");
-        
         flyWheelRight = hardwareMap.get(DcMotorEx.class, "fly1");
         flyWheelLeft = hardwareMap.get(DcMotorEx.class, "fly2");
-        
         //turret = hardwareMap.get(DcMotorEx.class, "turret");
-        
         driveMotorBL = hardwareMap.get(DcMotorEx.class, "driveBL");
         driveMotorFL = hardwareMap.get(DcMotorEx.class, "driveFL");
         driveMotorBR = hardwareMap.get(DcMotorEx.class, "driveBR");
@@ -88,29 +83,33 @@ public class owentestcode extends LinearOpMode {
             double oneRightStickX = - gamepad1.right_stick_x;
             double oneRightStickY = gamepad1.right_stick_y;
             double oneLeftStickX = gamepad1.left_stick_x;
-            
             double twoLeftStickY = gamepad2.left_stick_y;
-            
+
+            //boost
             if (gamepad1.right_bumper) {
                 boost = 1000;    
             } else {
                 boost = 0;
             }
-            
+
+            //drive
             driveMotorBL.setVelocity((driveSpeed + boost) * -(oneRightStickX - oneRightStickY + oneLeftStickX));
             driveMotorFL.setVelocity((driveSpeed + boost) * (oneRightStickX + oneRightStickY - oneLeftStickX));
             driveMotorBR.setVelocity((driveSpeed + boost) * -(oneRightStickX + oneRightStickY + oneLeftStickX));
             driveMotorFR.setVelocity((driveSpeed + boost) * (oneRightStickX - oneRightStickY - oneLeftStickX));
-            
+
+            //flywheel
             flyWheelLeft.setPower(-twoLeftStickY * flyWheelSpeed);
             flyWheelRight.setPower(twoLeftStickY * flyWheelSpeed);
-            
+
+            //intake
             if (gamepad2.left_bumper) {
                 intake.setPower(-1);
             } else {
                 intake.setPower(0);
             }
-            
+
+            //servos
              if (gamepad2.right_bumper) {
                 outZero.setPower(-1);
                 outOne.setPower(1);
